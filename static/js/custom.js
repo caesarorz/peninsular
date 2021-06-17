a073a733a16425aa5e606e1bd075c90b79881c90
Cesar Orozco <caesar.orz@gmail.com>
2021-06-17 11:17:43 -0600

    Add html Modal and readme.md
    Adding the modal for contactenos (like contact us). Still need to add
    closing functionality.
    Then the db and email.
    
    Adding readme file. Still need to work on that.
    
    Changes to be committed:
    	new file:   README.md
    	new file:   static/css/custom.css
    	modified:   static/js/custom.js
    	modified:   templates/base.html
    	modified:   templates/base/head.html
    	new file:   templates/modal/contact.html

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,10 @@
 const modalContactenos = () => {
-    console.log('contactenos')
+    const modal = document.getElementById('modal-contactenos')
+    //modal.classList.toggle("show");
+    //modal.classList.toggle("mystyle");
+    
+    console.log('contactenos ', modal)
+
 } 
 
 
