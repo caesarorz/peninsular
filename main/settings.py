16e161f2b65916584fc7c5f89314653d0ba586a8
Cesar Orozco <caesar.orz@gmail.com>
2021-06-18 16:35:51 -0600

    Changes to be committed: 	modified:   .gitignore 	modified:   main/settings.py
    Remove secret key and replace it with external file to work locally
    in settings: SECRET_KEY = getenv('SECRET_KEY', secret_key).

--- main/settings.py
+++ main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from os import getenv, path
 from pathlib import Path
-
+from .secret import secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-z0is#$qd7z&6)uhegpus0!ij&criqgs4)j3g@02zi4f9cd(q$@'
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = getenv('SECRET_KEY', secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('IS_DEVELOPMENT', True)
